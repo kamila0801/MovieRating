555a14936daaa9819fa96d0ef8c5450d2c05a462
soma199 <70133057+Laszlo199@users.noreply.github.com>
2021-09-28 13:13:57 +0200

    Implement 1-4

--- MovieRating.Domain/Services/RatingService.cs
+++ MovieRating.Domain/Services/RatingService.cs
@@ -1,36 +1,97 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using MovieRating.IServices;
+using MovieRating.Models;
 
 namespace MovieRating.Domain.Services
 {
     public class RatingService : IRatingService
     {
         private IReviewRepository _repo;
-        
+
         public RatingService(IReviewRepository repository)
         {
             _repo = repository;
         }
-        
+        //Test1
         public int GetNumberOfReviewsFromReviewer(int reviewer)
         {
-            throw new System.NotImplementedException();
-        }
+            List<Review> allReviews = _repo.ReadAll();
+            int i = 0;
 
+            foreach (var r in allReviews)
+            {
+                if (r.Reviewer == reviewer)
+                {
+                    i++;
+                }
+            }
+
+            return i;
+        }
+        //Test2
         public double GetAverageRateFromReviewer(int reviewer)
         {
-            throw new System.NotImplementedException();
+            List<Review> allReviews = _repo.ReadAll();
+            double total = 0;
+            int count = 0;
+            
+            foreach (var r in allReviews)
+            {
+                if (r.Reviewer == reviewer)
+                {
+                    total += r.Grade;
+                    count++;
+                }
+            }
+            
+            if (count == 0)
+            {
+                throw new InvalidDataException("This reviewer has no reviews");
+            }
+            
+            double result = total / count;
+            return result;
         }
-
+        //Test3
         public int GetNumberOfRatesByReviewer(int reviewer, int rate)
         {
-            throw new System.NotImplementedException();
+            List<Review> allReviews = _repo.ReadAll();
+            int count = 0;
+            
+            if (rate< 1 || rate > 5)
+            {
+                throw new InvalidDataException("The grade have to be between 1-5");
+            }
+            
+            foreach (var r in allReviews)
+            {
+                if (r.Reviewer == reviewer && r.Grade == rate)
+                {
+                    count++;
+                }
+            }
+            return count;
         }
-
+        //Test4
         public int GetNumberOfReviews(int movie)
         {
-            throw new System.NotImplementedException();
+            List<Review> allReviews = _repo.ReadAll();
+            int count = 0;
+            
+            foreach (var r in allReviews)
+            {
+                if (r.Movie == movie)
+                {
+                    count++;
+                }
+            }
+            if (count == 0)
+            {
+                throw new InvalidDataException("This movie has no reviews");
+            }
+            return count;
         }
 
         /// <summary>
---
+++ MovieRating.Test/TestOneToFour.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Moq;
+using MovieRating.Domain.Services;
+using MovieRating.IServices;
+using MovieRating.Models;
+using Xunit;
+
+namespace MovieRating.Test
+{
+    public class TestOneToFour
+    {
+        private readonly Mock<IReviewRepository> _mockRepo;
+        private readonly RatingService _service;
+
+        public TestOneToFour()
+        {
+            _mockRepo = new Mock<IReviewRepository>();
+            _service = new RatingService(_mockRepo.Object);
+            TestData();
+        }
+
+        public void TestData()
+        {
+            var rev1 = new Review
+            {
+                Reviewer = 1,
+                Movie = 2,
+                Grade = 3,
+                ReviewDate = new DateTime(2021, 3, 25)
+            };
+            var rev2 = new Review
+            {
+                Reviewer = 1,
+                Movie = 1,
+                Grade = 2,
+                ReviewDate = new DateTime(2021, 4, 23)
+            };
+            var rev3 = new Review
+            {
+                Reviewer = 2,
+                Movie = 2,
+                Grade = 1,
+                ReviewDate = new DateTime(2021, 7, 03)
+            };
+            var rev4 = new Review
+            {
+                Reviewer = 2,
+                Movie = 2,
+                Grade = 3,
+                ReviewDate = new DateTime(2017, 8, 23)
+            };
+            var rev5 = new Review
+            {
+                Reviewer = 3,
+                Movie = 3,
+                Grade = 4,
+                ReviewDate = new DateTime(2018, 10, 11)
+            };
+            var rev6 = new Review
+            {
+                Reviewer = 4,
+                Movie = 3,
+                Grade = 4,
+                ReviewDate = new DateTime(2020, 2, 12)
+            };
+            var list = new List<Review>()
+            {
+                rev1, rev2, rev3, rev4, rev5, rev6
+            };
+            _mockRepo.Setup(x => x.ReadAll()).Returns(list);
+        }
+
+
+        [Fact]
+        public void Test1()
+        {
+            int expect = 2;
+            int reviewer = 1;
+
+            int amount = _service.GetNumberOfReviewsFromReviewer(reviewer);
+            Assert.Equal(expect,amount);
+        }
+        
+        [Fact]
+        public void Test2()
+        {
+            double expect = 2.5;
+            int reviewer = 1;
+            
+            double result = _service.GetAverageRateFromReviewer(reviewer);
+            Assert.Equal(expect,result);
+        }
+        
+        [Fact]
+        public void Test3()
+        {
+            var expect = 1;
+            var reviewer = 1;
+            var input= 2;
+            
+            var result = _service.GetNumberOfRatesByReviewer(reviewer, input);
+            Assert.Equal(expect,result);
+        }
+        
+        [Fact]
+        public void Test4()
+        {
+            var expect = 3;
+            var input = 2;
+            
+            var result = _service.GetNumberOfReviews(input);
+            Assert.Equal(expect,result);
+        }
+    }
+}
